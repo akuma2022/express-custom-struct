8820c4ba2741535c97c155610e26177526a16285
hoang lam <lam.nh@oryza.vn>
2023-11-16 14:20:49 +0700

    first commit

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -1,11 +1,10 @@
 import compression from "compression";
-import dotenv from "dotenv";
+
 import express from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 
 const app = express();
-dotenv.config();
 
 // init middleware
 app.use(morgan("dev"));
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import "module-alias/register";
 import app from "@/routers";
+import dotenv from "dotenv";
 
+dotenv.config();
 app.listen(3000, () => {
   console.log("running on port 3000");
 });
